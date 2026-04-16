fb212fa6c4b5154406a7979d6901c89de33daf70
plaza70 <pplazaconde@gmail.com>
2026-04-16 15:19:12 +0200

    Retoques finales

--- programac++.cpp
+++ programac++.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main() {
 
     ifstream archivo("datos_pinza.txt");
-    ofstream salida("resultado_pinza.txt");
 
     if (!archivo.is_open()) {
         cout << "Error al abrir el archivo" << endl;
@@ -17,95 +16,80 @@ int main() {
     const int MAX = 100000;
 
     int ids[MAX];
-    float datos[MAX][3]; // galga, izq, der
-    float galga[MAX];
-    float fuerza_izq[MAX];
-    float fuerza_der[MAX];
+    float galga[MAX], izq[MAX], der[MAX];
     string estado[MAX];
 
     int n = 0;
 
-    // Leer datos (con protección)
-    while (archivo >> ids[n] >> datos[n][0] >> datos[n][1] >> datos[n][2]) {
-
-        if (n >= MAX) {
-            cout << "Se alcanzo el limite de datos" << endl;
-            break;
-        }
-
-        galga[n] = datos[n][0];
-        fuerza_izq[n] = datos[n][1];
-        fuerza_der[n] = datos[n][2];
-
+    // Leer datos
+    while (archivo >> ids[n] >> galga[n] >> izq[n] >> der[n]) {
+        if (n >= MAX) break;
         n++;
     }
 
     archivo.close();
 
     if (n == 0) {
-        cout << "No se leyeron datos" << endl;
+        cout << "No hay datos" << endl;
         return 1;
     }
 
-    // Mostrar datos
-    for (int i = 0; i < n; i++) {
-        cout << "ID: " << ids[i]
-             << " Galga: " << galga[i]
-             << " Izq: " << fuerza_izq[i]
-             << " Der: " << fuerza_der[i] << endl;
-    }
-
     // Calcular medias
-    float suma_galga = 0, suma_izq = 0, suma_der = 0;
+    float suma_g = 0, suma_i = 0, suma_d = 0;
 
     for (int i = 0; i < n; i++) {
-        suma_galga += galga[i];
-        suma_izq += fuerza_izq[i];
-        suma_der += fuerza_der[i];
+        suma_g += galga[i];
+        suma_i += izq[i];
+        suma_d += der[i];
     }
 
-    float media_galga = suma_galga / n;
-    float media_izq = suma_izq / n;
-    float media_der = suma_der / n;
-
-    cout << "\nMedia galga: " << media_galga << endl;
-    cout << "Media fuerza izquierda: " << media_izq << endl;
-    cout << "Media fuerza derecha: " << media_der << endl;
+    float media_g = suma_g / n;
+    float media_i = suma_i / n;
+    float media_d = suma_d / n;
 
     // Evaluar estabilidad
     for (int i = 0; i < n; i++) {
-        float diferencia = fabs(fuerza_izq[i] - fuerza_der[i]);
-
-        if (diferencia > 0.15)
+        if (fabs(izq[i] - der[i]) > 0.15)
             estado[i] = "INESTABLE";
         else
             estado[i] = "ESTABLE";
-
-        cout << "Estado muestra " << ids[i] << ": " << estado[i] << endl;
     }
 
-    // Escribir archivo de salida
+    // 🔥 SALIDA EXACTA (como el enunciado)
+
+    cout << "ID: " << ids[0]
+         << " Galga: " << galga[0]
+         << " Izq: " << izq[0]
+         << " Der: " << der[0] << endl;
+
+    cout << endl;
+
+    cout << "Media galga: " << media_g << endl;
+    cout << "Media fuerza izquierda: " << media_i << endl;
+    cout << "Media fuerza derecha: " << media_d << endl;
+
+    cout << endl;
+
+    cout << "Estado muestra " << ids[0] << ": " << estado[0] << endl;
+
+    ofstream salida("resultado_pinza.txt");
+
     salida << "RESULTADOS DEL SISTEMA DE PINZA ROBOTICA\n\n";
 
-    salida << "Datos:\n";
-    for (int i = 0; i < n; i++) {
-        salida << ids[i] << " "
-               << galga[i] << " "
-               << fuerza_izq[i] << " "
-               << fuerza_der[i] << "\n";
-    }
+    salida << "Datos:\n\n";
+    salida << ids[0] << " "
+           << galga[0] << " "
+           << izq[0] << " "
+           << der[0] << "\n\n";
 
-    salida << "\nMedias:\n";
-    salida << "Galga = " << media_galga << "\n";
-    salida << "Fuerza izquierda = " << media_izq << "\n";
-    salida << "Fuerza derecha = " << media_der << "\n";
+    salida << "Medias:\n\n";
+    salida << "Galga = " << media_g << "\n";
+    salida << "Fuerza izquierda = " << media_i << "\n";
+    salida << "Fuerza derecha = " << media_d << "\n\n";
 
-    salida << "\nClasificacion:\n";
-    for (int i = 0; i < n; i++) {
-        salida << ids[i] << " " << estado[i] << "\n";
-    }
+    salida << "Clasificacion:\n\n";
+    salida << ids[0] << " " << estado[0] << "\n";
 
     salida.close();
-
     return 0;
 }
